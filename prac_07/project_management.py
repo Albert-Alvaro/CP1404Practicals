a51f73ea30fb8b69b1148cfa9e9a470e354cb584
BlackWolf <albert.alvaro@my.jcu.edu.au>
2022-12-19 14:56:29 +0800

    created menu and some file processing parts

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -1,3 +1,5 @@
+from prac_07.project import Project
+
 MENU = """- (L)oad projects  
 - (S)ave projects  
 - (D)isplay projects  
@@ -5,3 +7,50 @@ MENU = """- (L)oad projects
 - (A)dd new project  
 - (U)pdate project
 - (Q)uit"""
+projects = []
+class_projects = []
+
+
+def main():
+    with open("projects.txt", "r", encoding="utf-8-sig") as in_file:
+        data = in_file.readlines()
+        extract_data(data)
+        add_to_class()
+        # choice = get_choice()
+        # while choice != "Q":
+        #     if choice == "L":
+        #         pass
+        #     elif choice == "S":
+        #         pass
+        #     elif choice == "D":
+        #         pass
+        #     elif choice == "F":
+        #         pass
+        #     elif choice == "A":
+        #         pass
+        #     elif choice == "U":
+        #         pass
+        #     else:
+        #         print("Invalid Choice")
+        #     choice = get_choice()
+
+
+def add_to_class():
+    for project in projects:
+        class_projects.append(Project(project[0], project[1], int(project[2]), float(project[3]), int(project[4])))
+
+
+def get_choice():
+    print(MENU, end="\n")
+    choice = input(">>>").upper()
+    return choice
+
+
+def extract_data(data):
+    for line in data[1:]:
+        items = line.strip().split("\t")
+        projects.append(items)
+
+
+main()
+
