6d2b3d25f2e11012fe49aada2b5beb3bd927eaae
Albert Alvaro <118112483+Albert-Alvaro@users.noreply.github.com>
2022-12-28 15:43:32 +0800

    created dynamic_labels

--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -1,17 +1,29 @@
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.uix.button import Button
+from kivy.uix.label import Label
 from kivy.properties import StringProperty
 
 
 class DynamicLabelsApp(App):
+    name_text = StringProperty()
+
     def __init__(self, **kwargs):
+        """Construct main app"""
         super().__init__(**kwargs)
-        # basic data (model) example - dictionary of names: phone numbers
-        self.name_to_phone = {}
+        self.names = ['Jack', 'Gary', 'Jiaqin', 'MingYu', 'Aditya', 'TanAnhNguyen', 'Albert', 'Alvaro']
 
     def build(self):
-        """Build the Kivy GUI."""
-        self.title = "Dynamic Widgets"
+        """Build the Kivy GUI"""
+        self.title = "Dynamic Labels"
         self.root = Builder.load_file('dynamic_labels.kv')
+        self.create_labels()
         return self.root
+
+    def create_labels(self):
+        """Creates label from entries in the list"""
+        for name in self.names:
+            temp_button = Label(text=name)
+            self.root.ids.label_box.add_widget(temp_button)
+
+
+DynamicLabelsApp().run()
